c6b56314b3c4391c4a680bdb20104006f567e946
Brawan89 <Rawan.Badr.elq.js@tuwaiq.edu.sa>
2021-12-06 20:03:52 +0300

    login reducer

--- task/src/reducers/login.js
+++ task/src/reducers/login.js
@@ -2,3 +2,38 @@ const instialState = {
     user: "",
     token: "",
 };
+
+//reduce function 
+const signIn = (state = instialState , action) => {
+    const {type , payload} = action;
+
+    switch(type) {
+        case "LOGIN":
+            const { user , token } = payload;
+            localStorage.setItem("token" , token);
+            return { user , token };
+
+        case "LOGOUT":
+            localStorage.clear();
+            return { user: null , token: "" };
+    
+        default: 
+            return state;
+    }
+}
+
+export default signIn;
+
+export const log = (data) => {
+    return {
+        type: "LOGIN",
+        payload: data
+    };
+};
+
+export const logout = (data) => {
+    return {
+        type: "LOGOUT",
+        payload: data, // { user: null , token: "" }
+    };
+};
